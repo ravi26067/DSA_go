6f0fa8b6241b189197460cd9e22b05730a94fa74
Ravi Kumar <ravi26067@gmail.com>
2024-05-05 02:16:54 +0530

    removed lld

--- LLD/MovieReview/routes/authRouter.go
+++
@@ -1,8 +0,0 @@
-package routes
-
-import "github.com/gin-gonic/gin"
-
-// AuthRoutes used for defining the api routes
-func AuthRoutes(router *gin.Engine) {
-	router.POST("users/signup", controllers.Signup())
-}
