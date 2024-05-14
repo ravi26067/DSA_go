040d5a6b4c5b63dba084f28bfae07fe2719cf5fd
Ravi Kumar <ravi26067@gmail.com>
2024-05-14 10:46:48 +0530

    Formatted

--- DataStructures/Arrays/set_matrix_zero.go
+++ DataStructures/Arrays/set_matrix_zero.go
@@ -1,45 +1,47 @@
+package main
+
 // Time : O(m*n) , Space: O(m+n)
 //https://leetcode.com/problems/set-matrix-zeroes/description/
-func setZeroes(matrix [][]int)  {
-    m:=len(matrix)
-    n:=len(matrix[0])
 
-    colmj:=1
-    for i:=range(m){
-        for j:=range(n){
-            if matrix[i][j]==0{
-                matrix[i][0] = 0
-                if j!=0{
-                    matrix[0][j] = 0
-                }else{
-                    colmj=0
-                }
-            }
-        }
-    }
-    for i:=1;i<m;i++{
-        for j:=1;j<n;j++{
-            if matrix[i][j]!=0{
-                if matrix[i][0]==0 || matrix[0][j]==0{
-                    matrix[i][j]=0
-                }
-            }
-        }
-    }
+func setZeroes(matrix [][]int) {
+	m := len(matrix)
+	n := len(matrix[0])
 
-    if matrix[0][0]==0{
-        for j:=range(n){
-            matrix[0][j]=0
-        }
-    }
-    if colmj==0{
-        for i:=range(m){
-            matrix[i][0]=0
-        }
-    }
+	colmj := 1
+	for i := range m {
+		for j := range n {
+			if matrix[i][j] == 0 {
+				matrix[i][0] = 0
+				if j != 0 {
+					matrix[0][j] = 0
+				} else {
+					colmj = 0
+				}
+			}
+		}
+	}
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			if matrix[i][j] != 0 {
+				if matrix[i][0] == 0 || matrix[0][j] == 0 {
+					matrix[i][j] = 0
+				}
+			}
+		}
+	}
+
+	if matrix[0][0] == 0 {
+		for j := range n {
+			matrix[0][j] = 0
+		}
+	}
+	if colmj == 0 {
+		for i := range m {
+			matrix[i][0] = 0
+		}
+	}
 
 }
-
 
 /*
 // Time : O(m*n) , Space: O(1)
